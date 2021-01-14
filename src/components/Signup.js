142168b9577d7bb708ff6aab496065785d7d410c
Deepak Biswal <deepak412@gmail.com>
2021-01-14 17:07:38 +0530

    Worked on signup feature

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,6 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Copyright from './Copyright';
+import { errorConfig } from '../configs/error';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -35,24 +36,54 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Signup() {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
+  const confirmPasswordRef = useRef();
   const { signup } = useAuth();
-  const [error, setError] = useState('');
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const history = useHistory();
   const classes = useStyles();
+  const { signupError } = errorConfig;
 
-  async function handleSubmit(e) {
+  const handleSignup = async e => {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Passwords do not match');
+    let error = {};
+
+    if (!firstNameRef.current.value || firstNameRef.current.value === '') {
+      error.firstName = signupError.firstName;
+    }
+
+    if (!lastNameRef.current.value || lastNameRef.current.value === '') {
+      error.lastName = signupError.lastName;
+    }
+
+    if (!emailRef.current.value || emailRef.current.value === '') {
+      error.email = signupError.email;
+    }
+
+    if (!passwordRef.current.value || passwordRef.current.value === '') {
+      error.password = signupError.password;
+    }
+
+    if (!confirmPasswordRef.current.value || confirmPasswordRef.current.value === '') {
+      error.confirmPassword = signupError.confirmPassword;
+    }
+
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      error.password = signupError.passwordNotMatch;
+      error.confirmPassword = signupError.passwordNotMatch;
+    }
+
+    if (Object.keys(error).length > 0) {
+      return setError(error);
     }
 
     try {
-      setError('');
+      setError(null);
       setLoading(true);
       await signup(emailRef.current.value, passwordRef.current.value);
       history.push('/');
@@ -61,7 +92,7 @@ export default function Signup() {
     }
 
     setLoading(false);
-  }
+  };
 
   return (
     <Container component='main' maxWidth='xs'>
@@ -73,7 +104,7 @@ export default function Signup() {
         <Typography component='h1' variant='h5'>
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSignup} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -85,6 +116,9 @@ export default function Signup() {
                 id='firstName'
                 label='First Name'
                 autoFocus
+                inputRef={firstNameRef}
+                error={error && error.firstName ? true : false}
+                helperText={error ? error.firstName : ''}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -96,6 +130,9 @@ export default function Signup() {
                 label='Last Name'
                 name='lastName'
                 autoComplete='lname'
+                inputRef={lastNameRef}
+                error={error && error.lastName ? true : false}
+                helperText={error ? error.lastName : ''}
               />
             </Grid>
             <Grid item xs={12}>
@@ -107,6 +144,9 @@ export default function Signup() {
                 label='Email Address'
                 name='email'
                 autoComplete='email'
+                inputRef={emailRef}
+                error={error && error.email ? true : false}
+                helperText={error ? error.email : ''}
               />
             </Grid>
             <Grid item xs={12}>
@@ -119,6 +159,24 @@ export default function Signup() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
+                inputRef={passwordRef}
+                error={error && error.password ? true : false}
+                helperText={error ? error.password : ''}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='confirmPassword'
+                label='Confirm Password'
+                type='password'
+                id='confirmPassword'
+                autoComplete='current-password'
+                inputRef={confirmPasswordRef}
+                error={error && error.confirmPassword ? true : false}
+                helperText={error ? error.confirmPassword : ''}
               />
             </Grid>
           </Grid>
---
+++ src/configs/error.js
@@ -0,0 +1,11 @@
+export const errorConfig = {
+  signupError: {
+    firstName: 'First name is required',
+    lastName: 'Last name is required',
+    email: 'Email address is required',
+    password: 'Password is required',
+    confirmPassword: 'Confirm password is required',
+    invalidEmail: 'Email address is not valid',
+    passwordNotMatch: 'Password do not match'
+  }
+};
